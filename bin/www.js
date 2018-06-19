cea4e8d456877758fe62c367dd053f8ecd3a31a6
user <user@users-Mac-mini-4.local>
2018-06-19 06:42:44 -0700

    change http://localhost to https://localhost

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 require('dotenv').config();
 const chalk = require('chalk');
-const http = require('http');
+const https = require('https');
 const app = require('../server');
-
+const fs = require('fs');
 const port = process.env.SHOPIFY_APP_PORT || '3000';
 app.set('port', port);
 
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync('/Volumes/Data/localhost.key'),
+  cert: fs.readFileSync('/Volumes/Data/localhost.cert'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
+const server = https.createServer(options, app);
 
 server.listen(port, err => {
   if (err) {
